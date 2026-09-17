087a3b4ffbd07d32344cc3d1fe29e489445865f7
fiona-strasser-p3 <fiona@p3analytics.nyc>
2026-09-17 07:28:24 -0400

    working on adding long/short emojis

--- src/basic_bot.py
+++ src/basic_bot.py
@@ -391,6 +391,7 @@ def build_calc_reply(ib, args, state):
     target = (base_capital * config.DEFAULT_CAPITAL_UTILIZATION) / margin_mult
     twice_base = leverage * short_notional
     net_delta = long_notional - leverage * short_notional
+    long_or_short = "long 🟢" if net_delta > 0 else "short 🔴"
  
     lines = [
         "*" + e(f"CURRENT PRICES FOR {short_ticker.upper()} & {long_ticker.upper()}") + "*",
@@ -410,6 +411,7 @@ def build_calc_reply(ib, args, state):
         e(f"long ${long_notional:,.2f} - leverage {leverage:g} x "
           f"short ${short_notional:,.2f}"),
         "*" + e(f"= ${net_delta:,.2f}") + "*",
+        e(f"Position is {long_or_short}")
         ]
     if derived_from == "long":
         lines.append(
@@ -561,6 +563,7 @@ def build_calcaction_reply(ib, args, state):
     target = (base_capital * config.DEFAULT_CAPITAL_UTILIZATION) / margin_mult
     twice_base = leverage * short_notional
     net_delta = long_notional - leverage * short_notional
+    long_or_short = "long 🟢" if net_delta > 0 else "short 🔴"
  
     lines = [
         "*" + e(f"CURRENT PRICES FOR {short_ticker.upper()} & {long_ticker.upper()}") + "*",
@@ -580,6 +583,7 @@ def build_calcaction_reply(ib, args, state):
         e(f"long ${long_notional:,.2f} - leverage {leverage:g} x "
           f"short ${short_notional:,.2f}"),
         "*" + e(f"= ${net_delta:,.2f}") + "*",
+        e(f"Position is {long_or_short}")
         ]
     if derived_from == "long":
         lines.append(
